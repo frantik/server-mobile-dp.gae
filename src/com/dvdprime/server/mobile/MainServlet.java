ae088ab568db1d17ac49a44d1f3e38d4999a31e6
frantik <frantik@gmail.com>
2013-06-20 09:40:52 +0900

    jersey 환경에 맞춰서 라이브러리 변경

--- src/com/dvdprime/server/mobile/MainServlet.java
+++ src/com/dvdprime/server/mobile/MainServlet.java
@@ -3,12 +3,18 @@ package com.dvdprime.server.mobile;
 import java.io.IOException;
 import javax.servlet.http.*;
 
+/**
+ * 기본 페이지
+ * 
+ * @author 작은광명
+ *
+ */
 @SuppressWarnings("serial")
-public class Server_mobile_dpServlet extends HttpServlet
+public class MainServlet extends HttpServlet
 {
     public void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException
     {
         resp.setContentType("text/plain");
-        resp.getWriter().println("Hello, world");
+        resp.getWriter().println("Hello, DvdPrime Mobile Server by frantik");
     }
 }
